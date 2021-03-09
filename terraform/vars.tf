f45cf853fb70c472354397c42dac10169fed5cb9
Mert Alnuaimi <snickns@gmail.com>
2021-03-09 10:08:46 +0300

    Update vars.tf

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -30,14 +30,10 @@ variable "ssh-key" {
 #}
 
 variable "subscription_id" {
-  default = "e90bd4d0-3b50-4a27-a7e8-bc88cf5f5398"
 }
 variable "client_id" {
-  default = "0c56ff6c-269a-4f8c-afcb-c9cc8ec96501"
 }
 variable "client_secret" {
-  default = "phyM5Xf7v~7p93.O1~TcpLNlwkHvJS~Z17"
 }
 variable "tenant_id" {
-  default = "1bef4fb2-f9cc-478e-ac6f-da7d49fcad00"
 }
